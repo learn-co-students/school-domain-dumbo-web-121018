3d7ed7be1cd54e72aa2d7062188f83a9e00e58b1
Menachem Aber <abermendel@gmail.com>
2018-12-12 22:26:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(grd)
+    @roster[grd]
+  end
+  
+  def sort
+    @roster.each do |k, v|
+      v.sort!
+    end
+    @roster
+  end
+  
+end
